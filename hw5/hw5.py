0ac85f43b0d06cfca6fa4949ecd95375cfe7798c
triolo <triolo@mail.ru>
2016-11-14 23:40:01 +0300

    исправлен порядок

--- hw5/hw5.py
+++ hw5/hw5.py
@@ -3,12 +3,12 @@ alist = list(a)
 m = []
 ind = 0
 while a:
+    alist = alist[::-1]
     for el in alist:
         if ind % 3 == 2:
             alist[ind] = ''
         ind += 1
     ares = ''.join(alist)
-    ares = ares[::-1]
     m.append(ares)
     a = input()
     alist = list(a)
